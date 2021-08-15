b35e1e7acb82d6343a783035ce6394de106b4838
Fernanda de Oliveira Carvalho <nandaoc@pop-os.localdomain>
2021-08-14 23:11:13 -0300

    inserindo id ao image view e fazendo-o receber o resultado da photoIntent

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MyProfileActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MyProfileActivity.java
@@ -2,13 +2,17 @@ package br.ufmg.coltec.tp.e04intents;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MyProfileActivity extends Activity {
+
+    private static int PHOTO_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,7 @@ public class MyProfileActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(photoIntent);
+                startActivityForResult(photoIntent, PHOTO_CODE);
             }
         });
 
@@ -48,5 +52,16 @@ public class MyProfileActivity extends Activity {
                 startActivity(share);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        ImageView imageView = findViewById(R.id.profile_photo);
+
+        //testa se o código passado como parâmetro é igual ao codigo da foto e se a execução ocorreu corretamente
+        if(requestCode == PHOTO_CODE && resultCode == Activity.RESULT_OK){
+            Bitmap profilePhoto = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(profilePhoto);
+        }
     }
 }
