52979934183ab6d210c4ef1dccfd38b54b747ad3
Fernanda de Oliveira Carvalho <nandaoc@pop-os.localdomain>
2021-08-14 23:22:08 -0300

    adicionando botão que aciona o leitor de código e implementando a funcionalidade de leitura

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -17,13 +17,33 @@ public class MainActivity extends Activity {
         btnMyProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast toast = Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT);
-//                toast.show();
                 Intent intent = new Intent(MainActivity.this, MyProfileActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
+        Button btnScanCode = findViewById(R.id.btn_scan_code);
+        btnScanCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        //testa se o código solicitado é 0, como foi setado na linha 31
+        if(requestCode == 0){
+            if(resultCode == RESULT_OK){ //testa se a execução ocorreu corretamente
+                // armazena  que foi codificado
+                String contentsEncoded = data.getStringExtra("SCAN_RESULT");
+                // armazena o tipo de código (numero, QRCOde, etc)
+                String codeFormat = data.getStringExtra("SCAN_RESULT_FORMAT");
+            }
+        }
     }
 }
