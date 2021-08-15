24c6421fbef22e2bb36ebe51d0ee401c899c2a56
Fernanda de Oliveira Carvalho <nandaoc@pop-os.localdomain>
2021-08-14 22:45:14 -0300

    alterando layout e adicionando funcionalidades aos botões

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -1,7 +1,10 @@
 package br.ufmg.coltec.tp.e04intents;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMyProfile = findViewById(R.id.btn_profile);
+        btnMyProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast toast = Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT);
+//                toast.show();
+                Intent intent = new Intent(MainActivity.this, MyProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MyProfileActivity.java
@@ -0,0 +1,52 @@
+package br.ufmg.coltec.tp.e04intents;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+public class MyProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+
+        Button btnChangePhoto = findViewById(R.id.btn_change_photo);
+        btnChangePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(photoIntent);
+            }
+        });
+
+        Button btnCallMe = findViewById(R.id.btn_phone_number);
+        btnCallMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uriPhoneNumber = Uri.parse("tel: 9999999999999");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, uriPhoneNumber);
+                startActivity(callIntent);
+            }
+        });
+
+        Button btnSendEmail = findViewById(R.id.btn_email_address);
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendEmailIntent = new Intent();
+                sendEmailIntent.setAction(Intent.ACTION_SEND);
+                sendEmailIntent.putExtra(Intent.EXTRA_EMAIL, "2020954820@teiacoltec.org");
+                sendEmailIntent.putExtra(Intent.EXTRA_TEXT, "Hello, Fernanda!");
+                sendEmailIntent.setType("text/plain");
+
+                Intent share = Intent.createChooser(sendEmailIntent, null);
+                startActivity(share);
+            }
+        });
+    }
+}
