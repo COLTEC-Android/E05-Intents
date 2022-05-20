5c0049aa413babe5d4467e9df4deb0978a60d8ad
BrendaSales <brendasales1997@gmail.com>
2022-05-19 21:39:20 -0300

    Finalização tela de perfil

--- app/src/main/java/com/example/e05_intents/MainActivity.java
+++ app/src/main/java/com/example/e05_intents/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.e05_intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int FOTO_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button fotoBtn = findViewById(R.id.btn_alterar_foto);
+        TextView txtEmail = findViewById(R.id.txt_email);
+        TextView txtTelefone = findViewById(R.id.txt_telefone);
+
+        txtTelefone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:31999999999");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        fotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(fotoIntent, FOTO_CODE);
+            }
+        });
+
+        txtEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("mailto:brenda@email.com");
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView imageView = findViewById(R.id.img_foto);
+
+        if (requestCode == FOTO_CODE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+
 }
