69c14d85dbd9eefa91f2c54f3098f6d5d9844139
pogum <samuel.pogum@gmail.com>
2022-05-19 22:15:11 -0300

    Android E05: Intents - Tela de Perfil - falta fazer a parte do e-mail

--- app/src/main/java/com/example/e05intents/MainActivity.java
+++ app/src/main/java/com/example/e05intents/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.e05intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int FOTO_CODE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton alterafoto = findViewById(R.id.foto);
+
+        alterafoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(fotoIntent,FOTO_CODE);
+            }
+        });
+
+        TextView text = findViewById(R.id.telefone);
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse ("tel:33181920");
+                Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView imageView = findViewById(R.id.foto);
+
+        if (requestCode == FOTO_CODE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+
+
 }
