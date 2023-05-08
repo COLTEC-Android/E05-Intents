57e284130deff73f9e0e6834f258176d0d39d817
Jeferson Junio <jjsconstantino@gmail.com>
2023-05-08 11:27:09 -0300

    Parte 1 Intents

--- app/src/main/java/com/example/e05_intents/MainActivity.java
+++ app/src/main/java/com/example/e05_intents/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.e05_intents;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    public static int FOTO_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv_number = findViewById(R.id.tv_number);
+        String numero = tv_number.getText().toString();
+        imageView = findViewById(R.id.img_photo);
+
+        tv_number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:" + numero);
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(fotoIntent, FOTO_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        imageView = findViewById(R.id.img_photo);
+
+        if (requestCode == FOTO_CODE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+
+        Toast.makeText(this, "Foto alterada!", Toast.LENGTH_SHORT).show();
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
