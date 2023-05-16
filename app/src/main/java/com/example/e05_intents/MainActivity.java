a6230d36fff29176c851fec5bd656492a149fcab
2022953430 <jjsconstantino@gmail.com>
2023-05-15 21:09:41 -0300

    Finalizacao do exercicio

--- app/src/main/java/com/example/e05_intents/MainActivity.java
+++ app/src/main/java/com/example/e05_intents/MainActivity.java
@@ -19,10 +19,13 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public static int FOTO_CODE = 1;
+    public static int SCANNER_CODE = 0;
 
     ImageView imageView;
     TextView tv_number;
     TextView tv_email;
+    TextView tv_codigo;
+    Button btn_scanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
         tv_number = findViewById(R.id.tv_number);
         imageView = findViewById(R.id.img_photo);
         tv_email = findViewById(R.id.tv_email);
+        btn_scanner = this.findViewById(R.id.btn_scanner);
+        btn_scanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                startActivityForResult(intent, SCANNER_CODE);
+            }
+        });
 
         tv_email.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,12 +77,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
         imageView = findViewById(R.id.img_photo);
+        tv_codigo = findViewById(R.id.txt_codigo);
 
         if (requestCode == FOTO_CODE && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
             Toast.makeText(this, "Foto alterada!", Toast.LENGTH_SHORT).show();
             
+        }
+
+        if (requestCode == SCANNER_CODE) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+                tv_codigo.setText(contents);
+
+            }
         }
 
         super.onActivityResult(requestCode, resultCode, data);
